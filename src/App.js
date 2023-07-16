510c80feb2a3583f08d9eefced6d1ce164542b18
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2023-07-16 10:02:49 +0530

    commit app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./app.css";
+import data from "./data.jsx";
+import "./card.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [sub, setSub] = useState(0);
+  const [selectedOption, setOp] = useState("");
+  const [result, setResult] = useState(0);
+  var optionsArray = data[sub] ? data[sub].options : [];
+
+  function handleChange(option) {
+    console.log("Selected Option:", option);
+    setOp(option);
+  }
+  
+  function handleSubmit() {
+    if (selectedOption === data[sub].answer) {
+      setResult(result + 1);
+    }
+    console.log("Current score is:", result);
+    setOp("");
+    setSub(sub + 1);
+  }
+
+  function resetQuiz() {
+    setSub(0);
+    setResult(0);
+  }
+  if (sub < data.length) {
+    return (
+      <div className="card">
+        <div className="question">
+          <span className="Number">{data[sub].id}) </span>
+          {data[sub].question}
+        </div>
+        <div className="options">
+          {optionsArray.map((item, index) => (
+            <React.Fragment key={index}>
+              <label htmlFor={item}>
+                <input
+                  type="radio"
+                  name="selectedAnswer"
+                  id={item}
+                  checked={selectedOption === item}
+                  onChange={() => handleChange(item)}
+                />
+                {item}
+              </label>
+              <br />
+            </React.Fragment>
+          ))}
+        </div>
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="show-result">
+        <span className="result-text">Hello there, You have successfully attended the quiz
+        <br/>Your Result is : {result} out of {data.length}</span><br/>
+        <button className="reset-quiz" type="submit" onClick={resetQuiz}>Restart the quiz</button>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/data.jsx
@@ -0,0 +1,64 @@
+const data = [
+    {
+      id: 1,
+      question: "What is capital of India?",
+      options:["Delhi", "kolkata", "Pune", "Mumbai"],
+      answer:"Delhi"
+    },
+    {
+      id: 2,
+      question: "Which country gifted the ‘Statue of Liberty’ to USA in 1886?",
+      options:["France", "Canada","Brazil", "England"],
+      answer:"France"
+    },
+    {
+      id: 3,
+      question: "Dead Sea is located between which two countries?",
+      options:["Jordan and Sudan", "Jordan and Israel", "Turkey and UAE", "UAE and Egypt"],
+      answer:"Jordan and Israel"
+    },
+    {
+      id: 4,
+      question: "In which ocean ‘Bermuda Triangle’ region is located?",
+      options:["Atlantic","Indian","Pacific","Arctic"],
+      answer:"Atlantic"
+    },
+    {
+      id: 5,
+      question: "Which country is known as the ‘playground of Europe’",
+      options:["Austria","Holland","Switzerland","Italy"],
+      answer:"Switzerland"
+    },
+    {
+      id: 6,
+      question: "Total number of oceans in the World is",
+      options:["3", "5", "7", "12"],
+      answer:"5"
+    },
+    {
+      id: 7,
+      question: "Which country is also known as the ‘Land of Thousand Lakes’",
+      options:["Norway","Finland","Switzerland","Italy"],
+      answer:"Finland"
+    },
+    {
+      id: 8,
+      question: "The world’s longest straight road without any corners is located in",
+      options:["USA","Australia","Saudi Arabia","China"],
+      answer:"Saudi Arabia"
+    },
+    {
+      id: 9,
+      question: "Which one is the biggest island in the World?",
+      options:["Borneo","Finland","Sumatra","Greenland"],
+      answer:"Greenland"
+    },
+    {
+      id: 10,
+      question: "Which one is the biggest island in the World?",
+      options:["1982","1989","1995","1997"],
+      answer:"1997"
+    }
+  ]
+  
+export default data;
